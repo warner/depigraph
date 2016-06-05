cdcd19ea8b60703c4de5dd57575ebb50aeeac46e
Brian Warner <warner@lothar.com>
2016-06-04 22:06:38 -0700

    make entry point work

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name="depigraph",
+    version="0.5.0",
+    description="Draw dependency graphs for python distributions",
+    author="Brian Warner",
+    author_email="warner-depigraph@lothar.com",
+    license="MIT",
+    url="https://github.com/warner/depigraph",
+    py_modules=["depigraph"],
+    entry_points={"console_scripts": ["depigraph = depigraph:go"]},
+    install_requires=["click"],
+    )
+
+# TODO: use pypi 'graphviz' to build the .dot file
